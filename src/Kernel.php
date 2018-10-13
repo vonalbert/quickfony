67f0d63f7b41b03c2010813f816ddb4e1d685f1d
Alberto Avon <alberto.avon@gmail.com>
2018-10-13 19:16:43 +0200

    Define all routes in routes/

--- src/Kernel.php
+++ src/Kernel.php
@@ -59,6 +59,5 @@ class Kernel extends BaseKernel
 
         $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
     }
 }
